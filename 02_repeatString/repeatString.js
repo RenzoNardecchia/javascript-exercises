d954489b20ac1b0473f5f34a2fe6a6dafe968f65
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-02 18:00:51 -0300

    Exercise 2 done!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringReturn = '';
+    let i = 0;
+    if(num < 0){
+        stringReturn = 'ERROR';
+    }
+    while(i < num){
+        stringReturn += string;
+        i++;
+    }
+    return stringReturn;
 };
 
 // Do not edit below this line
