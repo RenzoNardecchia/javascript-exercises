8a3c6aa11c2dab268a8d997dbb08bf38d87407fb
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-03 19:26:03 -0300

    Exercise 4 done!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = array;
+    for(let i = 0; i < args.length; i++){
 
+        for(let j = 0; j < newArray.length; j++){
+            if(newArray[j] === args[i]){
+                newArray.splice(j, 1);
+            } else continue;
+        }
+
+                    
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
