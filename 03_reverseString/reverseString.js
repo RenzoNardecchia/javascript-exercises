bd2f14dc133e19bff7c12470081849f0cad3db62
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-03 10:06:38 -0300

    Exercise 3 done!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayFromString = string.split('');
+    let reversedArray = arrayFromString.reverse();
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
