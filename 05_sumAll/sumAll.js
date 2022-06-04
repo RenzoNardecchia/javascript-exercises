7389c5b4203278e0a5f3e04d8c0a4d825cfe869e
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-03 21:04:12 -0300

    Exercise 05 done!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let numbersAdded = 0;
 
+    // Checks if the arguments aren't negative or aren't a number vv
+    if(num1 < 0 || num2 < 0) return 'ERROR';
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+
+    // The operation starts here vv
+    if (num1 < num2){
+        for(let i = num1; i <= num2; i++){
+            numbersAdded += i;
+        }
+    } else if (num1 > num2){
+        for(let i = num2; i <= num1; i++){
+            numbersAdded += i;
+        }
+    }
+
+    return numbersAdded;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
