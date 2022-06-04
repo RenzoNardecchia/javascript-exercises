6f4b684d4b368c24aed8907620dad7bdddd717f3
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-04 11:01:55 -0300

    Exercise 7 done!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(num1) {
+  let conversion = ((num1 - 32) * (5 / 9)).toFixed(1);
+  return +conversion;
+  
 };
 
-const ctof = function() {
-
+const ctof = function(num1) {
+  let conversion = (num1 * (9 / 5) + 32).toFixed(1);
+  return +conversion;
 };
 
 // Do not edit below this line
