d7d98557c525d95bc36d3d82fc3f5157bde83ef1
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-15 20:00:16 -0300

    exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let totalNumbers = numbers.reduce((total, num)=>{
+    return total + num;
+  }, 0);
+  return totalNumbers;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let totalNumber = numbers[0];
+    for(let i = 1; i < numbers.length; i++){
+        totalNumber *= numbers[i];
+    }
+  return totalNumber;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let totalNumber = 1;
+    for(let i = 1; i <= number; i++){
+        totalNumber *= i;
+    }
+    return totalNumber;
 };
 
 // Do not edit below this line
