167e90fd3cea364c46c8af783f997a95d6db0223
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-15 21:57:41 -0300

    exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let lowerCased = str.toLowerCase();
+    let stringArray = lowerCased.split('');
+    let filteredString = '';
+    for(let i = 0; i < stringArray.length; i++){
+        let currentCharcter = stringArray[i].charCodeAt(0);
+        if(currentCharcter < 65 || currentCharcter > 122 || (currentCharcter > 90 && currentCharcter < 97)){
+            continue;
+        }
+        filteredString += stringArray[i];
+    }
+    let filteredStringArray = filteredString.split('');
+    let reversedString = (filteredStringArray.reverse()).join('');
+    // console.log(reversedString);
+    if(filteredString === reversedString){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
