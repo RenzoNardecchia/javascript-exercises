d6582900149281b5ed379f8bc71405ef62aa421b
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-16 11:18:12 -0300

    exercise finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let num1 = 0;
+    let num2 = 1;
+    let nextTerm;
+    if (+number < 0){
+        return 'OOPS';
+    } else if(+number == 1){
+        return 1;
+    } else{
+        for (let i = 2; i <= +number; i++) {
+            nextTerm = num1 + num2;
+            num1 = num2;
+            num2 = nextTerm;
+        }
+        return nextTerm;
+    }
 };
 
 // Do not edit below this line
