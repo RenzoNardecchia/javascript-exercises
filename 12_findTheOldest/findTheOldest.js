c44bb456847641820be4a8e91e365302402a79c8
RenzoNardecchia <102700681+RenzoNardecchia@users.noreply.github.com>
2022-06-16 13:05:12 -0300

    Tested it in an HTML file and the results were correct. Still trying to figure out why the test fails. For now: exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldestGuy;
+    let oldestAge = 0;
+    for(let i = 0; i < array.length; i++){
+
+        if(array[i].yearOfDeath===undefined){array[i].yearOfDeath = 2022};
+
+        const guy = array[i].yearOfDeath - array[i].yearOfBirth;
+
+        if(oldestAge > guy){
+            continue;
+        }
+        oldestAge = guy;
+        oldestGuy = array[i].name;
+    }
+    for(let j = 0; j < array.length; j++){
+        if(array[j].name != oldestGuy){
+            array.splice([j], 1)
+        }
+    }
+    // return array[0].name;
+    
+    return oldestGuy;
 
 };
 
